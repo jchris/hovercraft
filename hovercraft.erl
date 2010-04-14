da1a365ac6b3d4ee0a6687513c9fcf4b4df18c08
Chris Anderson <jchris@apache.org>
2010-04-13 18:22:34 -0700

    thanks grempe for better docs

--- hovercraft.erl
+++ hovercraft.erl
@@ -81,7 +81,7 @@ open_db(DbName) ->
     couch_db:open(DbName, [?ADMIN_USER_CTX]).
 
 %%--------------------------------------------------------------------
-%% Function: db_info(DbName) -> {ok,created} | {error,Error}
+%% Function: db_info(DbName) -> {ok,Db} | {error,Error}
 %% Description: Gets the db_info as a proplist
 %%--------------------------------------------------------------------
 db_info(DbName) ->
@@ -89,8 +89,8 @@ db_info(DbName) ->
     couch_db:get_db_info(Db).
 
 %%--------------------------------------------------------------------
-%% Function: open_doc(DbName, DocId) -> {ok,created} | {error,Error}
-%% Description: Gets the eJSON form of the db_info
+%% Function: open_doc(DbName, DocId) -> {ok,Doc} | {error,Error}
+%% Description: Gets the eJSON form of the Document
 %%--------------------------------------------------------------------
 open_doc(DbName, DocId) ->
     {ok, Db} = open_db(DbName),
@@ -100,7 +100,7 @@ open_doc(DbName, DocId) ->
         
 %%--------------------------------------------------------------------
 %% Function: save_doc(DbName, Doc) -> {ok, EJsonInfo} | {error,Error}
-%% Description: Saves the doc the database, returns the id and rev
+%% Description: Saves the doc in the database, returns the id and rev
 %%--------------------------------------------------------------------
 save_doc(#db{}=Db, Doc) ->
     CouchDoc = ejson_to_couch_doc(Doc),
@@ -143,8 +143,8 @@ start_attachment(DbName, DocId, AName) ->
     {ok, Pid}.
 
 %%--------------------------------------------------------------------
-%% Function: start_attachment(DbName, DocId, AName) -> {ok, Pid} | {error,Error}
-%% Description: Starts a process to use for fetching attachment bytes
+q%% Function: next_attachment_bytes(Pid) -> {ok, done} | {error,Error}
+%% Description: Fetch attachment bytes
 %%--------------------------------------------------------------------
 next_attachment_bytes(Pid) ->
     Pid ! {next_attachment_bytes, self()},
